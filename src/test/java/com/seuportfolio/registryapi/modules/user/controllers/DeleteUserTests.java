c01f366b3ac999a7fd32e57262b8601c7103d89b
NicolasCBV <nicolascbv700@gmail.com>
2024-05-05 01:52:38 -0300

    feat: add profiles

--- src/test/java/com/seuportfolio/registryapi/modules/user/controllers/DeleteUserTests.java
+++ src/test/java/com/seuportfolio/registryapi/modules/user/controllers/DeleteUserTests.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -24,6 +25,7 @@ import com.seuportfolio.registryapi.modules.user.repositories.UserRepo;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class DeleteUserTests {
 	@Autowired
 	private MockMvc mockMvc;
--- src/test/java/com/seuportfolio/registryapi/modules/user/controllers/LoginTests.java
+++ src/test/java/com/seuportfolio/registryapi/modules/user/controllers/LoginTests.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -23,6 +24,7 @@ import com.seuportfolio.registryapi.modules.user.repositories.UserRepo;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class LoginTests {
 	@Autowired
 	private MockMvc mockMvc;
